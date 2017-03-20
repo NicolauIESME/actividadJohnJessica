2d51f6c8f596bb3b623d64681cfcfcf07f3de865
nicolau <nicolau@Ares>
2017-03-20 17:36:05 +0100

    Subiendo cambios en proyecto HolaMundo

--- HolaMundo/index.php
+++ HolaMundo/index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo"Pepito";
+        echo "© Copyright 2017"
         ?>
     </body>
 </html>
